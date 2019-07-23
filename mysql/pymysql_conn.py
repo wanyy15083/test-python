088ac359a97e43a5bc9f2523890ebb2db42f5067
songyigui <songyigui@weshare.com.cn>
2019-07-23 16:30:14 +0800

    add pymysql template

---
+++ mysql/pymysql_conn.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import pymysql
+
+my_config = {
+    'host': '127.0.0.1',
+    'port': 3306,
+    'user': 'root',
+    'password': '1234',
+    'db': 'test',
+    'charset': 'utf8mb4',
+    'cursorclass': pymysql.cursors.DictCursor
+}
+
+
+class MyPymysql():
+    """
+    MYSQL数据库对象，负责产生数据库连接 , 此类中的连接采用连接池实现获取连接对象：conn = Mysql.getConn()
+            释放连接对象;conn.close()或del conn
+    """
+
+    def __init__(self, config):
+        self._conn = pymysql.connect(**config)
+        self._cursor = self._conn.cursor()
+
+    def getAll(self, sql, param=None):
+        """
+        @summary: 执行查询，并取出所有结果集
+        @param sql:查询ＳＱＬ，如果有查询条件，请只指定条件列表，并将条件值使用参数[param]传递进来
+        @param param: 可选参数，条件列表值（元组/列表）
+        @return: result list(字典对象)/boolean 查询到的结果集
+        """
+        if param is None:
+            count = self._cursor.execute(sql)
+        else:
+            count = self._cursor.execute(sql, param)
+        if count > 0:
+            result = self._cursor.fetchall()
+        else:
+            result = False
+        return result
+
+    def getOne(self, sql, param=None):
+        """
+        @summary: 执行查询，并取出第一条
+        @param sql:查询ＳＱＬ，如果有查询条件，请只指定条件列表，并将条件值使用参数[param]传递进来
+        @param param: 可选参数，条件列表值（元组/列表）
+        @return: result list/boolean 查询到的结果集
+        """
+        if param is None:
+            count = self._cursor.execute(sql)
+        else:
+            count = self._cursor.execute(sql, param)
+        if count > 0:
+            result = self._cursor.fetchone()
+        else:
+            result = False
+        return result
+
+    def getMany(self, sql, num, param=None):
+        """
+        @summary: 执行查询，并取出num条结果
+        @param sql:查询ＳＱＬ，如果有查询条件，请只指定条件列表，并将条件值使用参数[param]传递进来
+        @param num:取得的结果条数
+        @param param: 可选参数，条件列表值（元组/列表）
+        @return: result list/boolean 查询到的结果集
+        """
+        if param is None:
+            count = self._cursor.execute(sql)
+        else:
+            count = self._cursor.execute(sql, param)
+        if count > 0:
+            result = self._cursor.fetchmany(num)
+        else:
+            result = False
+        return result
+
+    def insertMany(self, sql, values):
+        """
+        @summary: 向数据表插入多条记录
+        @param sql:要插入的ＳＱＬ格式
+        @param values:要插入的记录数据tuple(tuple)/list[list]
+        @return: count 受影响的行数
+        """
+        count = self._cursor.executemany(sql, values)
+        return count
+
+    def __query(self, sql, param=None):
+        if param is None:
+            count = self._cursor.execute(sql)
+        else:
+            count = self._cursor.execute(sql, param)
+        return count
+
+    def update(self, sql, param=None):
+        """
+        @summary: 更新数据表记录
+        @param sql: ＳＱＬ格式及条件，使用(%s,%s)
+        @param param: 要更新的  值 tuple/list
+        @return: count 受影响的行数
+        """
+        return self.__query(sql, param)
+
+    def insert(self, sql, param=None):
+        """
+        @summary: 更新数据表记录
+        @param sql: ＳＱＬ格式及条件，使用(%s,%s)
+        @param param: 要更新的  值 tuple/list
+        @return: count 受影响的行数
+        """
+        return self.__query(sql, param)
+
+    def delete(self, sql, param=None):
+        """
+        @summary: 删除数据表记录
+        @param sql: ＳＱＬ格式及条件，使用(%s,%s)
+        @param param: 要删除的条件 值 tuple/list
+        @return: count 受影响的行数
+        """
+        return self.__query(sql, param)
+
+    def begin(self):
+        """
+        @summary: 开启事务
+        """
+        self._conn.autocommit(0)
+
+    def end(self, option='commit'):
+        """
+        @summary: 结束事务
+        """
+        if option == 'commit':
+            self._conn.commit()
+        else:
+            self._conn.rollback()
+
+    def dispose(self, isEnd=1):
+        """
+        @summary: 释放连接池资源
+        """
+        if isEnd == 1:
+            self.end('commit')
+        else:
+            self.end('rollback')
+        self._cursor.close()
+        self._conn.close()
+
+
+if __name__ == '__main__':
+    mysql = MyPymysql(my_config)
+
+    sqlAll = "select * from person;"
+    result = mysql.getAll(sqlAll)
+    print(result)
+
+    sqlAll = "select * from person;"
+    result = mysql.getMany(sqlAll, 2)
+    print(result)
+
+    sqlAll = "select * from person where id = %s;"
+    result = mysql.getOne(sqlAll, [2,])
+    print(result)
+
+    # mysql.insert("insert into myTest.aa set a=%s", (1))
+
+    # 释放资源
+    mysql.dispose()
